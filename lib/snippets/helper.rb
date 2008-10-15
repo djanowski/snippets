17ea43f9d94ac15127eb33c4cef2e128f252006e
Damian Janowski <damian.janowski@gmail.com>
2008-10-15 00:55:50 -0200

    Safer Hash#merge.

--- lib/snippets/helper.rb
+++ lib/snippets/helper.rb
@@ -2,7 +2,7 @@ module Snippets
   module Helper
     def snippet(slug, options = {}, &block)
       text = capture(&block) if block_given?
-      Snippet.get(slug, {:text => text}.merge(options))
+      Snippet.get(slug, {:text => text}.merge(options || {}))
     end
   end
 end
